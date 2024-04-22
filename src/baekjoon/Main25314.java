0ef1dd60bf8db9062f5f08af14a28fff112d5d7b
82107 <tow369@naver.com>
2024-04-22 15:42:23 +0900

    baekjoon 25314

---
+++ src/baekjoon/Main25314.java
@@ -0,0 +1,15 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class Main25314 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 0; i < n / 4; i++) {
+            System.out.print("long ");
+        }System.out.println("int");
+    }
+}
+
+
