73989662b34033b6e2d5ad38e1df4d775195e4ee
82107 <tow369@naver.com>
2024-04-24 13:10:20 +0900

    baekjoon 2738

---
+++ src/baekjoon/Main2738.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main2738 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        int[][] a = new int[n][m];
+        int[][] b = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = s.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                b[i][j] = s.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j] + b[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
