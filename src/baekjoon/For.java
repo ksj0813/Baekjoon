0a3e02e48ad1eb92f9595249db268ca8523bf221
82107 <tow369@naver.com>
2024-07-17 21:11:38 +0900

    For

---
+++ src/baekjoon/For.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+public class For {
+    public static void main(String[] args) {
+        int[] ary = {10, 20, 30};
+        for (int data : ary) {
+            System.out.println(data);
+        }
+    }
+}
