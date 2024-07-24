989dfef3be37f05b507ae43a087fef9ddaaf2b55
82107 <tow369@naver.com>
2024-07-24 13:32:59 +0900

    Static

---
+++ src/baekjoon/Code10_03.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+public class Code10_03 {
+        static int plus(int v1, int v2) {
+            int result = v1 + v2;
+            return result;
+    }
+
+    public static void main(String[] args) {
+        int hap;
+        hap = plus(100, 200);
+        System.out.println("100과 200의 plus() 메서드 결과 : " + hap);
+    }
+}
